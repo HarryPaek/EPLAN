5348b0bb1c172d15097968a521329f6011155eb4
HarryPaek <paek.harry@gmail.com>
2017-03-29 00:52:02 +0900

    Configuration Tool Codes

---
+++ APITraining/Eplan.ConfigurationTools/ConfiguratuionHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eplan.ConfigurationTools
+{
+    public class ConfiguratuionHelper
+    {
+        public const string DefaultConfigFilePath = @".\Config\EPLAN_CustomConfig.config";
+
+        public static Configuration GetConfiguration(string configPath)
+        {
+            if (string.IsNullOrWhiteSpace(configPath) || !File.Exists(configPath))
+                configPath = DefaultConfigFilePath;
+
+            ExeConfigurationFileMap fileMap = new ExeConfigurationFileMap()
+            {
+                ExeConfigFilename = configPath
+            };
+      
+            return ConfigurationManager.OpenMappedExeConfiguration(fileMap, ConfigurationUserLevel.None);
+        }
+    }
+}
