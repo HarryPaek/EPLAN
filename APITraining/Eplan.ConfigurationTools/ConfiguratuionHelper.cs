5119b3fd392934098bc084c233793173e5937107
HarryPaek <paek.harry@gmail.com>
2017-04-04 11:58:27 +0900

    Add Configuration Functions

--- APITraining/Eplan.ConfigurationTools/ConfiguratuionHelper.cs
+++ APITraining/Eplan.ConfigurationTools/ConfiguratuionHelper.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,12 +11,12 @@ namespace Eplan.ConfigurationTools
 {
     public class ConfiguratuionHelper
     {
-        public const string DefaultConfigFilePath = @".\Config\EPLAN_CustomConfig.config";
+        static string _configFilePath = string.Empty;
 
         public static Configuration GetConfiguration(string configPath)
         {
             if (string.IsNullOrWhiteSpace(configPath) || !File.Exists(configPath))
-                configPath = DefaultConfigFilePath;
+                configPath = DefaultCustomConfigFilePath;
 
             ExeConfigurationFileMap fileMap = new ExeConfigurationFileMap()
             {
@@ -23,6 +24,30 @@ namespace Eplan.ConfigurationTools
             };
       
             return ConfigurationManager.OpenMappedExeConfiguration(fileMap, ConfigurationUserLevel.None);
+        }
+
+        public static string DefaultCustomConfigFilePath
+        {
+            get { return @".\Config\EPLAN_CustomConfig.config"; }
+        }
+
+        public static string GetExecutingAssemblyConfigFileName
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_configFilePath))
+                {
+                    Assembly exeAssembly = Assembly.GetExecutingAssembly();
+                    _configFilePath = string.Format("{0}.config", exeAssembly.ManifestModule.Name);
+                }
+
+                return _configFilePath;
+            }
+
+            internal set
+            {
+                _configFilePath = value;
+            }
         }
     }
 }
